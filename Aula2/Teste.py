d70c4aee9792ee1edd4dd1d5826045edd39e8747
GS-suzuki13 <gustavosuzuki3@gmail.com>
2019-11-19 10:13:47 -0300

    Web

---
+++ Aula2/Teste.py
@@ -0,0 +1,10 @@
+pessoa = "", "Carinhoso", " Atecioso", " Querido"
+# print(pessoa)
+# print((' Nem ').join(pessoa))
+frase = " Gustavo Suzuki"
+# print('a'.join())
+
+print(pessoa[1:3])
+print(frase[9:])
+print(frase[9:14])
+print(pessoa.count(' carinhoso'.strip().capitalize()))
