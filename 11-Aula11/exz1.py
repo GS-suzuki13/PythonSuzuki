1fa858421aea21406e3927967ac5c11253b0b90a
GS-suzuki13 <gustavosuzuki3@gmail.com>
2020-01-07 10:51:36 -0300

    HBResolve

--- 11-Aula11/exz1.py
+++
@@ -1,5 +0,0 @@
-from iss import iss
-valorservico = float (input ('Digite o valor do servico:'))
-ISS= float (input ('Digite o valor a aliquota:'))
-
-print(f'O valor de imposto é:{iss(valorservico, ISS)} ')
--- 20-Aula20/debug.py
+++
@@ -1,9 +0,0 @@
-
-
-def imprime_isso(isso):
-    print(isso)
-
-imprime_isso([1, 2, 3])
-
-for i in [1, 2, 3, 4]:
-    imprime_isso(i)
